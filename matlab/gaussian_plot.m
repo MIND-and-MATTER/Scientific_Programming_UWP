762518552b5eabb408f6d26362d33e58d9a10752
lucca001 <lucca001@mainlab81.ad.uwp.edu>
2021-04-01 15:38:03 -0500

    Trial

---
+++ matlab/gaussian_plot.m
@@ -0,0 +1,24 @@
+x = 0:0.01:10;
+mu = 5;
+
+sig_1 = 0.5;
+ex_1 = -(x - mu).^2 / (2 * sig_1^2);
+graph_1 = exp(ex_1) / (sig_1 * sqrt(2 * pi));
+
+
+sig_2 = 1.0;
+ex_2 = -(x - mu).^2 / (2 * sig_2^2);
+graph_2 = exp(ex_2) / (sig_2 * sqrt(2 * pi));
+
+sig_3 = 1.5;
+ex_3 = -(x - mu).^2 / (2 * sig_3^2);
+graph_3 = exp(ex_3) / (sig_3 * sqrt(2 * pi));
+
+
+plot(x, graph_1,"-", x, graph_2,"-.", x, graph_3,".-");
+axis(010 
+
+
+
+
+
