0f62b9857cbfcc4b63d028f4321beccd90704e3d
lucca001 <lucca001@mainlab15.ad.uwp.edu>
2021-04-03 00:07:55 -0500

    fifth_eigenvector - complete

--- matlab/fifth_eigenvector.m
+++ matlab/fifth_eigenvector.m
@@ -1,1 +1,11 @@
-% Test comment
+A = 1+ ones(1,5);
+m = diag(A);
+m(1,2) = -1; m(2,1)=-1; m(2,3)=-1;  m(3,2)=-1;  m(3,4)=-1;
+m(4,3)=-1;  m(4,5)=-1;  m(5,4)=-1;
+
+H = ((5+1)^2 / 2) .* m
+
+y_values = eig(H);
+x_values = linspace(1/(5+1), 5/(5+1), 5);
+
+plot(x_values, y_values)
