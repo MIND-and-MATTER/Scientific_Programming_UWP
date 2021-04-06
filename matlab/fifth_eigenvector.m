851503f2684943684e9ef5573e28edcf52f7e05b
lucca001 <60672054+lucca001@users.noreply.github.com>
2021-04-06 15:50:56 -0500

    Update fifth_eigenvector.m

--- matlab/fifth_eigenvector.m
+++ matlab/fifth_eigenvector.m
@@ -2,11 +2,11 @@ A = 1+ ones(1,5);
 m = diag(A);
 
 for i = 1:4
-    m(i, i+1) = -1
-    m(i+1, i)= -1
+    m(i, i+1) = -1;
+    m(i+1, i)= -1;
 end
 
-H = ((5+1)^2 / 2) .* m;
+H = ((5+1)^2 / 2) * m;
 
 [eigenvectors, eigenvalues] = eig(H);
 x_values = linspace(1/(5+1), 5/(5+1), 5);
