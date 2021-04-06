61cc44ec62cef58174698acb167851fb31a951c2
lucca001 <60672054+lucca001@users.noreply.github.com>
2021-04-06 15:41:48 -0500

    Update tenth_eigenvector.m

--- matlab/tenth_eigenvector.m
+++ matlab/tenth_eigenvector.m
@@ -2,22 +2,16 @@ A = 1 + ones(1, 10);
 m = diag(A);
 for i = 1:9
     m(i, i+1) = -1;
-end
-
-for j = 1:9
-    m(j+1, j) = -1;
+    m(i+1, i) = -1;
 end
 
 H = ((10+1)^2 / 2) .* m;
 
 n_points = 10;
-y_values = eig(H);
+[eigenvectors, eigenvalues] = eig(H);
 x_values = linspace(1/(n_points+1), n_points/(n_points+1), n_points);
 y2 = sqrt(2) * (sin(pi * x_values));
 
-plot(x_values, y_values, "-", x_values, y2, "-." )
-hold on
-axis([0 1])
-hold off
+plot(x_values, eigenvectors(:,1), "-", x_values, y2, "-." )
 
     
