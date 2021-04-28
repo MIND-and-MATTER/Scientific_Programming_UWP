924349299b65603d3e193e9d13dc2458657dbfe2
lucca001 <60672054+lucca001@users.noreply.github.com>
2021-04-28 11:59:11 -0500

    Update Maxwell_Distribution.m
    Modified for suggestions

--- matlab/Maxwell_Distribution.m
+++ matlab/Maxwell_Distribution.m
@@ -1,9 +1,43 @@
+%{
+This program is designed to take a user input of Kelvin temperature. Data relating to molecular gasses will be
+extracted from a file. Calculations will return probability functions for the velocities of gas particles at the
+given temperature. Computations to output a graphical representation of these functions will occur and the results
+ will be displayed.
+ %}
+
+
+
 T = 300;
-Data = readcell("Gas_masses.csv");
+k = 1.38064852e-23;
+Data = readcell("Gas_mass.csv");
 Names = Data(:,1);
 Masses = Data(:,2);
 Velocity  = zeros(length(Names), 3);
 Legen = string(zeros(length(Names), 1));
+
+
+
+
+function [velo, dist] = distribution_curve( T, m)
+    v = (0: 5: 1000);
+    A = zeros(2, length(v));
+    fact =  (4*pi) * (m / (2*pi*k*T))^(3/2);
+    for i = 1: length(v)
+        A(1, i) = v(i);
+        A(2, i) = fact * v(i)^2 * exp(-(m * (v(i)^2)) / (2*k*T));
+
+    end
+    velo = A(1,:);
+    dist = A(2,:);
+end
+
+
+function [v_max, v_rms, v_ave] = velocities(T, m)
+    fact = sqrt(k*T / m);
+    v_max = sqrt(2) * fact;
+    v_rms = sqrt(3) * fact;
+    v_ave = sqrt(8 / pi) * fact;
+end
 
 hold on
 for i = 1:length(Names)
@@ -22,39 +56,4 @@ xlabel("Velocity (m/s)")
 ylabel("Probability")
 hold off
 
-
-%function[] = Maxwell_Distribution(T, m)
-
-
-%function [Data] = extract(file_name)
- %   E = importdata(file_name);
-  %  Names = [E.textdata];
-   % Masses = [E.data]./ 6.0221409e26;
-    %Data = containers.Map(Names, Masses);      
-%end
-
-
-function [velo, dist] = distribution_curve( T, m)
-    k = 1.38064852e-23;
-    v = (0: 5: 1000);
-    A = zeros(2, length(v));
-    fact =  (4*pi) * (m / (2*pi*k*T))^(3/2);
-    for i = 1: length(v)
-        A(1, i) = v(i);
-        A(2, i) = fact * v(i)^2 * exp(-(m * (v(i)^2)) / (2*k*T));
-
-    end
-    velo = A(1,:);
-    dist = A(2,:);
 end
-
-
-function [v_max, v_rms, v_ave] = velocities(T, m)
-    k = 1.38064852e-23;
-    fact = sqrt(k*T / m);
-    v_max = sqrt(2) * fact;
-    v_rms = sqrt(3) * fact;
-    v_ave = sqrt(8 / pi) * fact;
-end
-
-%end
