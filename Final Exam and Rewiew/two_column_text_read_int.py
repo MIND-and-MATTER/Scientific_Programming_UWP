5aba423e780a19dc395f1d7a35628fd170c8bab1
lucca001 <lucca001@rangers.uwp.edu>
2021-05-04 20:54:49 -0500

    Adjustments made to make x values "smart."

--- Final Exam and Rewiew/two_column_text_read_int.py
+++ Final Exam and Rewiew/two_column_text_read_int.py
@@ -1,1 +1,17 @@
+def two_column_text_read(file_name):
+    import numpy as np
+
+    file = open(file_name)
+    content = file.readlines()
+    data = np.zeros([2, (len(content))], float)
+
+    for line in content:
+        elements = line.split()
+        data[0, line-1] = float(elements[0])
+        data[1, line-1] = float(elements[0])
+
+    return data
+
+
+print(two_column_text_read("volumes_energies.dat"))
 
