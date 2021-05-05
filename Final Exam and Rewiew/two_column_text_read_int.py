847698c630b3a4f80331cf5e8a5208f0b1e56bd8
lucca001 <lucca001@rangers.uwp.edu>
2021-05-04 21:35:39 -0500

    Final Review Progress

--- Final Exam and Rewiew/two_column_text_read_int.py
+++ Final Exam and Rewiew/two_column_text_read_int.py
@@ -1,17 +1,31 @@
+import numpy as np
+
 def two_column_text_read(file_name):
-    import numpy as np
 
     file = open(file_name)
     content = file.readlines()
     data = np.zeros([2, (len(content))], float)
+    n = 0
 
     for line in content:
         elements = line.split()
-        data[0, line-1] = float(elements[0])
-        data[1, line-1] = float(elements[0])
+        data[0, n] = float(elements[0])
+        data[1, n] = float(elements[1])
+        n += 1
 
     return data
 
 
+def quadratic_fit(array):
+
+    x_values = array[0, :]
+    y_values = array[1, :]
+
+    quadratic_coefficients = np.polyfit(x_values, y_values, 3)
+
+    return quadratic_coefficients
+
+
 print(two_column_text_read("volumes_energies.dat"))
+print(quadratic_fit(two_column_text_read("volumes_energies.dat")))
 
