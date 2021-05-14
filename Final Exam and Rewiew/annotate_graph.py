e06d53f8967fafefa1121f329c1648a16aae47b6
lucca001 <lucca001@rangers.uwp.edu>
2021-05-13 21:27:40 -0500

    Final Progress

--- Final Exam and Rewiew/annotate_graph.py
+++ Final Exam and Rewiew/annotate_graph.py
@@ -6,11 +6,14 @@ def annotate_graph(file_name_string, modulus):
     plt.annotate("Created by Michael Luccas 2020-05-12",
                  xy=(10, 10), xycoords='figure pixels')
     plt.annotate(file_name_string[0],
-                 xy=(10, 460), xycoords='figure pixels')
+                 xy=(120, 380), xycoords='figure pixels')
     plt.annotate(file_name_string[1],
                  xy=(340, 100), xycoords='figure pixels')
     plt.annotate("$K_0$ ="+str(round(modulus, 3)),
                  xy=(340, 115), xycoords='figure pixels')
-    plt.title("")
+    plt.annotate("Birch-Murnaghan Equation of State for "+file_name_string[0]+" in DFT "+file_name_string[2],
+                xy=(130, 460),
+                xycoords='figure pixels'
+                )
 
 
